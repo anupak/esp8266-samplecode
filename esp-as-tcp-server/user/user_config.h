26587586fff4dec428f4cff528b55ded22716053
Anup Kulkarni <you@example.com>
2015-08-28 16:49:58 +0530

    Removed the SSID/Password

--- esp-as-tcp-server/user/user_config.h
+++ esp-as-tcp-server/user/user_config.h
@@ -4,8 +4,8 @@
 
 typedef enum {SIG_DO_NOTHING=0, SIG_START_SERVER=1, SIG_SEND_DATA, SIG_UART0} USER_SIGNALS;
 
-#define SSID            "ADSNet24g"
-#define PASSWORD        "WhereAreyouAnup"
+#define SSID            "YourSSID"
+#define PASSWORD        "YourPassword"
 #define SERVER_IP		"192.168.1.15"
 #define SERVER_PORT		7777
 
